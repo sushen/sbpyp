922d3abead41f598f10a3bbc5a0a5099ccba821e
Sushen Biswas <sushenbiswasaga@gmail.com>
2022-07-02 06:08:48 +0600

    add: Lots of file

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='number_utility',
+    version='0.0.3',
+    description='The number-utility module makes it simple for you to do number manipulation and perform various operations on numbers.',
+    py_modules=["number_utility"],
+    package_dir={'': 'src'},
+    extras_require={
+        "dev": [
+            "pytest >= 3.7",
+            "check-manifest",
+            "twine"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Sushen Biswas",
+    author_email="sushenbiswasaga@gmail.com",
+    url="https://github.com/sushen/sbpyp"
+)
---
+++ test_number_utility.py
@@ -0,0 +1,47 @@
+from number_utility import *
+import pytest
+
+
+def test_number_utility_reverse_num():
+    assert reverse_num(345) == 543
+
+
+def test_number_utility_sum_of_digits():
+    assert sum_of_digits(748) == 19
+
+
+def test_number_utility_is_prime():
+    assert is_prime(17) == True
+
+
+def test_number_utility_generate_primes():
+    assert generate_primes(5, 50) == [
+        5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43]
+
+
+def test_number_utility_gcd():
+    assert gcd(12, 60) == 12
+
+
+def test_number_utility_lcm():
+    assert lcm(5, 6) == 30
+
+
+def test_number_utility_get_factors():
+    assert get_factors(10) == [1, 10, 2, 5]
+
+
+def test_number_utility_factorial():
+    assert factorial(6) == 720
+
+
+def test_number_utility_fibonacci():
+    assert fibonacci(6) == 5
+
+
+def test_number_utility_number_of_digits():
+    assert number_of_digits(153) == 3
+
+
+def test_number_utility_is_armstrong():
+    assert is_armstrong(1634) == True
